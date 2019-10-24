1d0fd68baf515553f68edbb492c9611d807b9ab6
Obada Alabbadi <abadiobada@gmail.com>
2019-10-24 19:32:13 +0300

    initial commit

---
+++ src/Algorithms.java
@@ -0,0 +1,22 @@
+public class Algorithms
+{
+    public void sort(int[] a)
+    {
+        for (int i = 0; i < a.length; i++)
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (a[j + 1] < a[j])
+                {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
